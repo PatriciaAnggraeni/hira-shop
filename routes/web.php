f1b4c2faf54513895c35769ede0b66de426cfa51
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-06-14 09:49:13 +0700

    buat menu dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Auth::routes();
-
